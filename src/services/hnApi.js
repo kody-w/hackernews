e209f83c1eb79e7b73577cf72692535757bb1bd4
Kody Wildfeuer <kwildfeuer@alphabold.com>
2019-11-21 06:45:06 -0800

    Created Story component

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,11 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
-export const getStoryIds = async () => {
-    const result = await axios.get(newStoriesUrl).then(({ data }) => data)
+export const getStory = async storyId => {
+  const result = await axios.get(`${storyUrl + storyId}.json`).then(({ data }) => data);
 
-    return result
-}
+  return result;
+};
+
+export const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl).then(({ data }) => data);
+
+  return result;
+};
