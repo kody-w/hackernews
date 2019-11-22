ac4a7da205db9fc5982d53a73d4bef8046c4602c
Kody Wildfeuer <kwildfeuer@alphabold.com>
2019-11-22 07:05:22 -0800

    Added h1 tag and styled components package

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { getStoryIds, getStory } from "../services/hnApi";
-import { Story } from '../components/Story'
+import { Story } from "../components/Story";
 
 export const StoriesContainer = () => {
-    const [storyIds, setStoryIds] = useState([]);
-  
-    useEffect(() => {
-      getStoryIds().then(data => setStoryIds(data))
-    }, []);
-  
-    return storyIds.map(storyId => <Story storyId={storyId} />)
-  }
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
+
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+      {storyIds.map(storyId => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </>
+  );
+};
