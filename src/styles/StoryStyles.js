21e10b915e823b90f3cb372fc687d1fe1715cff8
Kody Wildfeuer <kwildfeuer@alphabold.com>
2019-11-25 21:01:45 -0800

    Updated styling

--- src/styles/StoryStyles.js
+++ src/styles/StoryStyles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const StoryWrapper = styled.section`
 padding-top: 10px;
@@ -13,4 +13,40 @@ border-top: 1px solid #cccccc;
     margin-bottom: 0;
     padding-bottom: 0;
 }
+`;
+
+export const StoryTitle = styled.h1`
+  margin-bottom: 5px;
+  font-size: 18px;
+  line-height: 1.8;
+  margin: 0;
+  text-decoration: none;
+
+  a {
+    color: #2e2e2c;
+    background-color: #f8dc3d;
+    text-decoration: none;
+  }
+`;
+
+export const StoryMeta = styled.div`
+  font-style: italic;
+
+  > span:first-child {
+      margin-right: 10px;
+  }
+
+  > span:non(:first-child):before {
+    content: '•'
+    margin: 0 7px;
+  }
+
+  .story__meta-bold {
+      font-weight: bold;
+  }
+`;
+
+export const StoryMetaElement = styled.span`
+  font-weight: bold;
+  color: ${props => props.color || "red"};
 `;
